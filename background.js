0dca07b84761d3a7c711c23de7000233ddd026f6
Jefferson Scher <jscher2000@gmail.com>
2018-04-07 16:48:20 -0700

    Version 0.4

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 /* 
   Copyright 2018. Jefferson "jscher2000" Scher. License: MPL-2.0.
-  Revision 0.3
+  Revision 0.3 - revise and prepopulate data structure
+  Revision 0.4 - add window/global switch, context menu items on toolbar button
 */
 
 /**** Create and populate data structure ****/
 
-var oTabs = {};
-
 // TODO make this user configurable
-var maxTabs = 5;
+var maxTabs = 5;      // maximum tabIds to store per window
+var maxGlobal = 10;   // maximum tabIds to store across all windows
+
+var oTabs = {};
 
 // Initialize oTabs object with up to maxTabs recent tabs per window
 browser.tabs.query({}).then((arrAllTabs) => {
@@ -17,8 +19,8 @@ browser.tabs.query({}).then((arrAllTabs) => {
 	// Store tabIds to oTabs
 	var i, arrWTabs;
 	for (i=0; i<arrAllTabs.length; i++){
-		// Add to global list for i=0 through maxTabs-1 (order among windows is unpredictable)
-		if (i < maxTabs) {
+		// Add to global list for i=0 through maxGlobal-1 (order among windows is unpredictable)
+		if (i < maxGlobal) {
 			if (!('global' in oTabs)) {
 				oTabs['global'] = [arrAllTabs[i].id];
 			} else {
@@ -59,7 +61,7 @@ browser.tabs.query({}).then((arrAllTabs) => {
 				// not in list, add to front
 				arrWTabs.unshift(arrQTabs[0].id);
 				// check length and trim off the last if too long
-				if (arrWTabs.length > maxTabs) arrWTabs.pop();
+				if (arrWTabs.length > maxGlobal) arrWTabs.pop();
 				// store change
 				oTabs['global'] = arrWTabs;
 			} else {
@@ -94,7 +96,7 @@ browser.tabs.onActivated.addListener((info) => {
 		// not in list, add to front
 		arrWTabs.unshift(info.tabId);
 		// check length and trim off the last if too long
-		if (arrWTabs.length > maxTabs) arrWTabs.pop();
+		if (arrWTabs.length > maxGlobal) arrWTabs.pop();
 		// store change
 		oTabs['global'] = arrWTabs;
 	} else {
@@ -184,7 +186,7 @@ browser.windows.onFocusChanged.addListener((wid) => {
 			// not in list, add to front
 			arrWTabs.unshift(foundtabs[0].id);
 			// check length and trim off the last if too long
-			if (arrWTabs.length > maxTabs) arrWTabs.pop();
+			if (arrWTabs.length > maxGlobal) arrWTabs.pop();
 			// store change
 			oTabs['global'] = arrWTabs;
 		} else {
@@ -197,18 +199,28 @@ browser.windows.onFocusChanged.addListener((wid) => {
 
 /**** Set up toolbar button listener and button/tooltip toggler ****/
 
+var blnSameWindow = true;
+
 // Listen for button click and switch to previous tab
 browser.browserAction.onClicked.addListener((currTab) => {
-	// Within window switch
-	var arrWTabs = oTabs[currTab.windowId];
-	if (arrWTabs.length > 1) {
-		browser.tabs.update(arrWTabs[1], {active:true});
+	if (blnSameWindow) {
+		// Within window switch
+		doSwitch(currTab.windowId, currTab.windowId);
 	} else {
-		console.log('NO TAB SWITCH: No previous tabid available for this window');
+		// Unrestricted switch
+		doSwitch('global', currTab.windowId);
 	}
 });
+
+function doSwitch(oKey, wid){
+	var arrWTabs = oTabs[oKey];
+	if (arrWTabs.length > 1) {
+		browser.tabs.update(arrWTabs[1], {active:true}).then((currTab) => {
+			if (currTab.windowId != wid) browser.windows.update(currTab.windowId, {focused: true});
+		});
+	}
+}
 // TODO add context menu or options or other magic to enable both within window and global switching
-// TODO add context menu list (last accessed tab history) -- different permissions
 
 // Set icon image and tooltip based on ability to switch within current window
 function setButton(wid){
@@ -222,3 +234,50 @@ function setButton(wid){
 		browser.browserAction.setTitle({title: 'Last Accessed Tab Not Available'}); 
 	}
 }
+
+/**** Toolbar Button context menu ****/
+
+browser.menus.create({
+  id: "bamenu_switchwindow",
+  title: "Go to Last Tab in This Window",
+  contexts: ["browser_action"]
+});
+
+browser.menus.create({
+  id: "bamenu_switchglobal",
+  title: "Go to Last Tab Anywhere",
+  contexts: ["browser_action"]
+});
+
+browser.menus.create({
+  id: "bamenu_chk_window",
+  type: "checkbox",
+  title: "Set Button Action: Same Window",
+  contexts: ["browser_action"],
+  checked: blnSameWindow
+});
+
+browser.menus.create({
+  id: "bamenu_endseparator",
+  type: "separator",
+  contexts: ["browser_action"]
+});
+
+browser.menus.onClicked.addListener((menuInfo, currTab) => {
+	switch (menuInfo.menuItemId) {
+		case 'bamenu_switchwindow':
+			// Within window switch
+			doSwitch(currTab.windowId, currTab.windowId);
+			break;
+		case 'bamenu_switchglobal':
+			// Unrestricted switch
+			doSwitch('global', currTab.windowId);
+			break;
+		case 'bamenu_chk_window':
+			if (menuInfo.checked) blnSameWindow = true;
+			else blnSameWindow = false;
+			break;
+	}
+});
+
+// TODO add context menu list (last accessed tab history) -- different permissions
